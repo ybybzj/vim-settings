09815cf3949381a6c0a02b412d0e629bec7b0768
jackzj <ybybzj@gmail.com>
2024-02-01 17:13:47 +0800

    update plugins

--- lua/my/plugins.lua
+++ lua/my/plugins.lua
@@ -190,6 +190,7 @@ return {
 				filetypes = {
 					javascript = true, -- allow specific filetype
 					typescript = true, -- allow specific filetype
+					zig = true,
 					["*"] = false, -- disable for all other filetypes and ignore default `filetypes`
 				},
 				suggestion = { enabled = false },
