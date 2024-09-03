d6004dd525baf5f1c004ddf3e424d244d978d8e9
jackzj <ybybzj@gmail.com>
2024-09-03 15:08:58 +0800

    update neoscroll config

--- lua/my/utils.lua
+++ lua/my/utils.lua
@@ -2,12 +2,5 @@
 require("zen-mode").setup({})
 require("twilight").setup()
 
-
 -- smooth scroll --
-require("neoscroll").setup()
-local t = {}
-t["<C-u>"] = { "scroll", { "-vim.wo.scroll", "true", "150" } }
-t["<C-d>"] = { "scroll", { "vim.wo.scroll", "true", "100" } }
-t["<C-b>"] = { "scroll", { "-vim.api.nvim_win_get_height(0)", "true", "150" } }
-t["<C-f>"] = { "scroll", { "vim.api.nvim_win_get_height(0)", "true", "150" } }
-require("neoscroll.config").set_mappings(t)
+require("neoscroll").setup({})
