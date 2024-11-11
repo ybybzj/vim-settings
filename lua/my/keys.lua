af2175b54b85ed0e3a806f633e034ca54d8a3b65
jackzj <ybybzj@gmail.com>
2024-11-11 17:05:02 +0800

    add command for clear unwanted floating popups

--- lua/my/keys.lua
+++ lua/my/keys.lua
@@ -47,3 +47,14 @@ map("n", "<leader>gg", "<cmd>lua _git_toggle()<CR>", { silent = true })
 
 -- clear notifications --
 map("n", "<leader>cc", "<cmd>lua require('notify').dismiss()<CR>", { silent = true })
+
+-- clear floating popups --
+vim.api.nvim_create_user_command("CloseFloatingWindows", function(opts)
+	for _, window_id in ipairs(vim.api.nvim_list_wins()) do
+		-- If window is floating
+		if vim.api.nvim_win_get_config(window_id).relative ~= "" then
+			-- Force close if called with !
+			vim.api.nvim_win_close(window_id, opts.bang)
+		end
+	end
+end, { bang = true, nargs = 0 })
