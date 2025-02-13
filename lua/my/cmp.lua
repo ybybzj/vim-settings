482ffabe52adfb6b50ce34ada09a5c0b0421c37f
ybybzj <ybybzj@gmail.com>
2025-02-13 10:12:27 +0800

    refine cmdline auto-completion in blink config

--- lua/my/cmp.lua
+++ lua/my/cmp.lua
@@ -50,7 +50,23 @@ local opts = {
 	},
 
 	completion = {
-		menu = { border = "rounded" },
+		list = {
+			selection = {
+				preselect = function(ctx)
+					return ctx.mode ~= "cmdline"
+				end,
+				auto_insert = function(ctx)
+					return ctx.mode ~= "cmdline"
+				end,
+			},
+		},
+		menu = {
+			border = "rounded",
+			-- Don't show completion menu automatically when searching
+			auto_show = function(ctx)
+				return ctx.mode ~= "cmdline" or not vim.tbl_contains({ "/", "?" }, vim.fn.getcmdtype())
+			end,
+		},
 		documentation = { window = { border = "rounded" } },
 	},
 
