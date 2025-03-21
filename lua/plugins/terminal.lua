00e4581e6a55a0ec275a2e07fa32cedfa30459a3
ybybzj <ybybzj@gmail.com>
2025-03-21 12:51:01 +0800

    fix <esc> override in lazygit terminal

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -1,14 +1,3 @@
-function _G.set_terminal_keymaps()
-	local opts = { buffer = 0 }
-	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
-	-- vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
-	-- vim.keymap.set("t", "<C-h>", [[<Cmd>wincmd h<CR>]], opts)
-	-- vim.keymap.set("t", "<C-j>", [[<Cmd>wincmd j<CR>]], opts)
-	-- vim.keymap.set("t", "<C-k>", [[<Cmd>wincmd k<CR>]], opts)
-	-- vim.keymap.set("t", "<C-l>", [[<Cmd>wincmd l<CR>]], opts)
-	-- vim.keymap.set("t", "<C-w>", [[<C-\><C-n><C-w>]], opts)
-end
-
 return {
 	{
 		"akinsho/toggleterm.nvim",
@@ -24,6 +13,17 @@ return {
 				end,
 				direction = "horizontal",
 				close_on_exit = true,
+				on_open = function(term)
+					if term.display_name ~= "Lazygit" then
+						vim.api.nvim_buf_set_keymap(
+							term.bufnr,
+							"t",
+							"<esc>",
+							[[<C-\><C-n>]],
+							{ noremap = true, silent = true }
+						)
+					end
+				end,
 			})
 			local Terminal = require("toggleterm.terminal").Terminal
 			local git_term = Terminal:new({
@@ -31,13 +31,10 @@ return {
 				cmd = "lazygit",
 				hidden = true,
 				hide_numbers = true,
-				shade_filetypes = {},
-				shade_terminals = true,
-				shading_factor = 2,
 				start_in_insert = true,
 				insert_mappings = true,
 				persist_size = true,
-				direction = "tab",
+				direction = "float",
 				close_on_exit = true,
 				shell = vim.o.shell,
 				float_opts = {
@@ -53,8 +50,6 @@ return {
 			_toggle_lazygit = function()
 				git_term:toggle()
 			end
-
-			vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 		end,
 		keys = {
 			{ "<space>gg", "<cmd>lua _toggle_lazygit()<CR>", desc = "GitUI" },
